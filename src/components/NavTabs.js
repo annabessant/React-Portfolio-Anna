1535b0006709684b1fcc463d16a5a0f34e872913
annabessant <anna@thefutureofinnovation.org>
2023-03-08 22:54:20 +0000

    revised starter code to project, home and contact pages

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -26,16 +26,7 @@ function NavTabs({ currentPage, handlePageChange }) {
           Projects
         </a>
       </li>
-      <li className="nav-item">
-        <a
-          href="#blog"
-          onClick={() => handlePageChange('Blog')}
-          // Check to see if the currentPage is `Blog`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-          className={currentPage === 'Blog' ? 'nav-link active' : 'nav-link'}
-        >
-          Blog
-        </a>
-      </li>
+      
       <li className="nav-item">
         <a
           href="#contact"
