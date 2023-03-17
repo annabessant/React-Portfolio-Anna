cb6e388a63810bd657a40128eed38f1db1c65730
annabessant <117825430+annabessant@users.noreply.github.com>
2023-03-17 18:34:57 +0000

    added projects on project page

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,17 +1,79 @@
 import React from 'react';
+import { Container, Row, Col } from "react-bootstrap";
+let Project1 = "/assets/images/Project_1_Password_Generator.png";
+
+
 
 export default function Projects() {
   return (
     <div>
-      <h1>Projects Page</h1>
-      <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
-      </p>
+      {/* Portfolio Section */}
+      <section className="PortfolioSection">
+      <Container>
+          <h2>Recent Projects</h2>
+          <p>"A journey of thousand miles begins with a single step" (Lao Tsu, 571 BC). These are mine beginner's steps.</p>
+        <Row className="ProjectsList">
+        <Col sm="4" className="Project">                 
+          <h4 class="project-title">Password Generator 🔑</h4>
+          <p className='info-p'> An app that generates a password when the button is clicked with 10-64 characters of lower-, upper-, numeric and/or special case.
+          </p>
+          <img height="350px" width="350px" src={process.env.PUBLIC_URL+Project1} alt="Project_1_Password_Generator.png" /> 
+          <a href="https://github.com/annabessant/Password-Generator" target="_blank" class="project-link">GitHub Repo</a>
+          <br></br>
+          <a href="https://annabessant.github.io/Password-Generator/" target="_blank" class="project-link">Deployment</a>
+        </Col>
+        <Col sm="4" className="Project">                 
+          {/* <img src={Project2} alt="Dynamics Agilty" /> */}
+          <h4 class="project-title">Bootstrap portfolio 🧑‍🦳</h4>
+          <p className='info-p'> Using Bootstrap to style your portfolio and determine how your layout behaves on different viewport sizes.
+          </p>
+          <a href="https://github.com/annabessant/Bootstrap_Portfolio" target="_blank" class="project-link">GitHub Repo</a>
+          <br></br>
+          <a href="https://annabessant.github.io/Bootstrap_Portfolio/" target="_blank" class="project-link">Deployment</a>
+        </Col> 
+        <Col sm="4" className="Project">                
+          {/* <img src={Project5} alt="Evo Design Studio" /> */}
+          <h4 class="project-title">My Team 🎶</h4>
+          <p className='info-p'> Takes in information about employees on a team, then generates an HTML webpage that displays summaries for each person.
+          </p>
+          <a href="https://github.com/annabessant/Team-Profile-Generator-with-Tests" target="_blank" class="project-link">GitHub Repo</a>
+          <br></br>
+          <a href="https://watch.screencastify.com/v/vStn9RCoqWpeMxQx0om8" target="_blank" class="project-link">Demo Video 🎥</a> 
+        </Col>       
+        </Row>
+        <Row className='pt-4'>
+        <Col sm="4" className="Project">    
+          {/* <img src={Project3} alt="counselor jyoti" /> */}
+          <h4 class="project-title">5 Day Weather Forecast 🌨️</h4>
+          <p className='info-p'> Shows weather conditions for a searched city and presents data with a 5-day forecast.
+          </p>
+          <a href="https://github.com/annabessant/Server-APIs-Weather-Dashboard" target="_blank" class="project-link">GitHub Repo</a>
+          <br></br>
+          <a href="https://annabessant.github.io/Server-APIs-Weather-Dashboard/" target="_blank" class="project-link">Deployment</a>
+      </Col>
+      <Col sm="4" className="Project">                
+          {/* <img src={Project4} alt="Car Galaxy" /> */}
+          <h4 class="project-title">ReadMe Generator💻</h4>
+          <p className='info-p'> Generates a professional README.md file from a user's input using the Inquirer package.
+          </p>
+          <a href="https://github.com/annabessant/Node.js-Professional-README-Generator" target="_blank" class="project-link">GitHub Repo</a>
+          <br></br>
+          <a href="https://watch.screencastify.com/v/zXTWfzLMnpdB0AbBDkT5" target="_blank" class="project-link">Demo Video 🎥</a>
+      </Col>
+      <Col sm="4" className="Project">                
+          {/* <img src={Project5} alt="Evo Design Studio" /> */}
+          <h4 class="project-title">Movie project🎞️</h4>
+          <p className='info-p'> Searches for movies, shows a trailer, stores. Done with Matt Gaarder & Milda Jancauskaite.
+          </p>
+          <a href="https://github.com/MattGaarder/moovie-project" target="_blank" class="project-link">GitHub Repo</a>
+          <br></br>
+          <a href="https://mattgaarder.github.io/moovie-project/" target="_blank" class="project-link">Deployment</a>  
+      </Col>
+        </Row>
+      </Container>
+    </section>
+
+    {/* <Footer /> */}
     </div>
-  );
+  )
 }
